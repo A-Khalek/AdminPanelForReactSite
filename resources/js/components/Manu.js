8f2c8ae941ae760d252dd3ab5189d16bc1e77003
A-Khalek <khalek.raju.bd@gmail.com>
2020-12-02 12:49:04 +0600

    Initial commit

---
+++ resources/js/components/Manu.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Manu extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Laravel + react app</h1>
+            </div>
+        );
+    }
+}
+
+export default Manu;
