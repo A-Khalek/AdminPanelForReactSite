d1199e259714d76b18ef592c5a7d4c74e92c177a
A-Khalek <khalek.raju.bd@gmail.com>
2020-12-02 22:46:29 +0600

    add navBar and sidenavbar css

--- resources/js/components/Manu.js
+++ resources/js/components/Manu.js
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {Navbar} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 class Manu extends Component {
     render() {
         return (
-            <div>
-                <h1>New Laravel + react app</h1>
-            </div>
-        );
+            <Fragment>
+                <Navbar expand="lg" className="fixed-top" variant="light" bg="light">
+                    <Navbar.Brand href="#home"><FontAwesomeIcon icon={faBars}/> React-Bootstrap </Navbar.Brand>
+                </Navbar>
+
+            </Fragment>
+        )
     }
 }
 
